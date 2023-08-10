47240dced73b190d2d3d88195305738820cd0a44
Vladimir Mokshin <v.m.mokshin@gmail.com>
2023-08-10 10:52:18 -0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,23 +4,36 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
     plugins: [react(), VitePWA({
-        // includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+        registerType: 'autoUpdate',
+        includeAssets: ['favicon.ico', 'apple-touch-icon-180x180.png', 'maskable-icon-512x512.svg'],
         manifest: {
-            name: 'My Awesome App',
-            short_name: 'MyApp',
-            description: 'My Awesome App description',
+            name: 'My PWA',
+            short_name: 'MyPWA',
+            description: 'My Awesome PWA',
             theme_color: '#ffffff',
             icons: [
                 {
-                    "src": "android/android-launchericon-512-512.png",
-                    "sizes": "512x512",
-                    "type": "image/png"
+                    src: 'pwa-64x64.png',
+                    sizes: '64x64',
+                    type: 'image/png'
                 },
                 {
-                    "src": "android/android-launchericon-192-192.png",
-                    "sizes": "192x192",
-                    "type": "image/png"
+                    src: 'pwa-192x192.png',
+                    sizes: '192x192',
+                    type: 'image/png'
                 },
+                {
+                    src: 'pwa-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                    purpose: 'any'
+                },
+                {
+                    src: 'maskable-icon-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                    purpose: 'maskable'
+                }
             ]
         }
     })],
