2075129711d73ab2db1578f8c620fdf01d3292c4
Vladimir Mokshin <v.m.mokshin@gmail.com>
2023-08-10 11:41:26 -0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
         registerType: 'autoUpdate',
         includeAssets: ['favicon.ico', 'apple-touch-icon-180x180.png', 'maskable-icon-512x512.svg'],
         manifest: {
+            id: 'MyPWA',
             name: 'My PWA',
             short_name: 'MyPWA',
             description: 'My Awesome PWA',
@@ -34,7 +35,20 @@ export default defineConfig({
                     type: 'image/png',
                     purpose: 'maskable'
                 }
-            ]
+            ],
+            "display_override": [
+                "window-controls-overlay",
+                "standalone",
+                "browser"
+            ],
+            "edge_side_panel": {
+                "preferred_width": 400
+            },
+            "orientation": "any",
+            "handle_links": "preferred",
+            "launch_handler": {
+                "client_mode": "navigate-existing"
+            }
         }
     })],
     build: {
