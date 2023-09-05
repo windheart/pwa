83c0f5287ecfecf2bf9186ad05a33fbb50f88ded
Vladimir Mokshin <v.m.mokshin@gmail.com>
2023-09-05 10:10:08 -0700

    barcode test

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
 
     useEffect(() => {
         scannerRef.current = new Html5QrcodeScanner(
-            "reader", { fps: 10, useBarCodeDetectorIfSupported: true }, false)
+            "reader", { fps: 10, qrbox: 500, useBarCodeDetectorIfSupported: true }, false)
 
         scannerRef.current.render(onScanSuccess, onScanFailure);
     }, []);
